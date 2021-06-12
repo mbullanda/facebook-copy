f9dc6570f89510cbf929b2caa48affcbf9b00da7
mbull <mbullanda@gmail.com>
2021-06-12 10:38:59 +0200

    create project- facebook copy

---
+++ src/main/java/pl/michal/facebookcopy/FacebookCopyApplication.java
@@ -0,0 +1,13 @@
+package pl.michal.facebookcopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookCopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookCopyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/michal/facebookcopy/FacebookCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michal.facebookcopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookCopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
